9a69e9fab6fe1bbd652f84dfa3056924517738ad
hamedpro <hamedyaghootpour@gmail.com>
2022-07-05 04:16:16 -0700

    backup

--- api/uploadServer.js
+++ api/uploadServer.js
@@ -1,16 +1,24 @@
-var http = require('http');
+var express = require('express')
 var formidable = require('formidable');
 var fs = require('fs');
+var cors = require('cors')
 
-http.createServer(function (req, res){
+var app = express()
+app.use(cors())
+
+app.all('/',(req,res)=>{
   var form = new formidable.IncomingForm();
   form.parse(req, function (err, fields, files){
     var oldpath = files.filetoupload.filepath;
     var newpath =  "./" + files.filetoupload.originalFilename;
-    fs.copyFile(oldpath, newpath, function (err) {
+    fs.copyFile(oldpath, "./" + newpath, function (err) {
       if (err) throw err;
       res.write('File was uploaded!');
       res.end();
     })
   })  
-}).listen(2000);
+})
+
+app.listen(2000,()=>{
+  console.log('app started listening on port 2000 ')
+})
